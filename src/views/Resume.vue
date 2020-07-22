9a5078ca500be7e20b12b9be500875d732ad2afe
Colin <colintklein@gmail.com>
2020-07-22 12:46:32 -0500

    Updates HTML for resume vue

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -14,40 +14,31 @@
 
     <h2>Skills</h2>
     <div v-for="skill in student.skills">
-      Skill {{ skill.name }}
+      {{ skill.name }}
     </div>
 
     <h2>Capstone</h2>
-    <!-- <div v-for="capstone_item in student.capstone"> -->
-    <div>
-      <h4>name | url</h4>
-      <p>description</p>
-      <!-- <h4>{{ capstone_item.name }} | {{ capstone_item.url }}</h4>
-      <p>{{ capstone_item.description }}</p> -->
+    <div v-for="capstone_item in student.capstone">
+      <h4>{{ capstone_item.name }} | {{ capstone_item.url }}</h4>
+      <p>{{ capstone_item.description }}</p>
       <img :src="capstone_item.screenshot" :alt="capstone_item.name">
     </div>
 
     <h2>Experience</h2>
-    <!-- <div v-for="experience_item in student.experience"> -->
-    <div>
-      <h4>jobtitle | companyname | startdate - enddate</h4>
-      <p>details</p>
-      <!-- <h4>{{ experience_item.job_title }} | {{ experience_item.company_name }} | 
+    <div v-for="experience_item in student.experience">
+      <h4>{{ experience_item.job_title }} | {{ experience_item.company_name }} | 
       {{ experience_item.start_date }} - 
       {{ experience_item.end_date }} </h4>
-      <p>{{ experience_item.details }}</p> -->
+      <p>{{ experience_item.details }}</p>
     </div>
 
 
     <h2>Education</h2>
-    <!-- <div v-for="education_item in student.education"> -->
-    <div>
-      <h4>degree | universityname | startdate - enddate</h4>
-      <p>details</p>
-      <!-- <h4>{{ experience_item.degree }} | {{ experience_item.university_name }} | 
-      {{ experience_item.start_date }} - 
-      {{ experience_item.end_date }} </h4>
-      <p>{{ experience_item.details }}</p> -->
+    <div v-for="education_item in student.education">
+      <h4>{{ education_item.degree }} | {{ education_item.university_name }} | 
+      {{ education_item.start_date }} - 
+      {{ education_item.end_date }} </h4>
+      <p>{{ education_item.details }}</p>
     </div>
 
   </div>
@@ -59,7 +50,7 @@
 <script>
 import axios from "axios";
 export default {
-  data: function () {
+  data: function() {
     return {
       student: {
         first_name: "Cheddar",
@@ -69,7 +60,7 @@ export default {
         short_bio: "I'm great in macaroni and cheese!",
         linkedin_url: "http://linkedin.com/in/cheddar",
         twitter_handle: "http://twitter.com/cheddar",
-        github_url: "http://github/com/cheddar",
+        github_url: "http://github.com/cheddar",
         personal_url: "http://cheddar.com/",
         image:
           "https://www.nutritionadvance.com/wp-content/uploads/2018/05/Cheddar-Cheese-101.jpg",
@@ -77,7 +68,7 @@ export default {
           { name: "Ruby" },
           { name: "Rails" },
           { name: "HTML" },
-          { name: "react" },
+          { name: "react" }
         ],
         education: [
           {
@@ -85,22 +76,22 @@ export default {
             end_date: "Feb. 22, 2019",
             degree: "Associates",
             university_name: "Community Cheese College",
-            details: "Studied development of cheese",
+            details: "Studied development of cheese"
           },
           {
             start_date: "Mar. 1, 2019",
             end_date: "Mar. 22, 2019",
             degree: "Bachelors",
             university_name: "Cheese College",
-            details: "Studied development of cheese",
+            details: "Studied development of cheese"
           },
           {
             start_date: "May. 1, 2019",
             end_date: "July. 28, 2019",
             degree: "Masters",
             university_name: "Cheese University",
-            details: "Master Cheese",
-          },
+            details: "Master Cheese"
+          }
         ],
         experience: [
           {
@@ -108,45 +99,45 @@ export default {
             end_date: "Sept. 22, 2019",
             job_title: "Cheese Developer",
             company_name: "Oberwise",
-            details: "Milked cows",
+            details: "Milked cows"
           },
           {
             start_date: "Nov.8, 2019",
             end_date: "Dec. 22, 2019",
             job_title: "Cheese Developer",
             company_name: "Dean's Dairy Farm",
-            details: "Formed cheese",
+            details: "Formed cheese"
           },
           {
             start_date: "Jan.8, 2019",
             end_date: "Feb. 22, 2019",
             job_title: "Cheese Engineer",
             company_name: "Horizon",
-            details: "Managed cheese makers",
-          },
+            details: "Managed cheese makers"
+          }
         ],
         capstone: [
           {
             name: "Local Cheese App",
             description: "find local cheese sources based on your location",
             url: "http://wheredacheeseat.com",
-            screenshot: "https://cheeseapp.jpg",
+            screenshot: "https://cheeseapp.jpg"
           },
           {
             name: "Say Cheese",
             description: "Cheese filters for cameras",
             url: "http://saycheese.com",
-            screenshot: "https://saycheese.jpg",
-          },
-        ],
-      },
+            screenshot: "https://saycheese.jpg"
+          }
+        ]
+      }
     };
   },
-  created: function () {
-    axios.get(`/api/students/${this.$route.params.id}`).then((response) => {
+  created: function() {
+    axios.get(`/api/students/${this.$route.params.id}`).then(response => {
       this.student = response.data;
     });
   },
-  methods: {},
+  methods: {}
 };
 </script>
