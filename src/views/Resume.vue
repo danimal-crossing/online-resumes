0e4f85afa330a09795d0ec54d9442f3f1560aa7c
Colin <colintklein@gmail.com>
2020-07-23 12:23:17 -0500

    Updates variables and sample data to match terms used by api team

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,14 +3,14 @@
     <img :src="student.image" :alt="student.first_name">
     <h1>{{ student.first_name }} {{ student.last_name }}</h1>
     <p>
-      {{ student.email}} |
+      {{ student.email }} |
       {{ student.phone_number }} |
       {{ student.linkedin_url }} |
       {{ student.github_url}} |
       {{ student.twitter_handle }} |
-      {{ student.personal_url }}
+      {{ student.website_url }}
     </p>
-    <h3>{{ student.short_bio }}</h3>
+    <h4>{{ student.bio }}</h4>
 
     <h2>Skills</h2>
     <div v-for="skill in student.skills">
@@ -18,27 +18,27 @@
     </div>
 
     <h2>Capstone</h2>
-    <div v-for="capstone_item in student.capstone">
-      <h4>{{ capstone_item.name }} | {{ capstone_item.url }}</h4>
-      <p>{{ capstone_item.description }}</p>
-      <img :src="capstone_item.screenshot" :alt="capstone_item.name">
+    <div v-for="capstone in student.capstones">
+      <h4>{{ capstone.name }} | {{ capstone.url }}</h4>
+      <p>{{ capstone.description }}</p>
+      <img v-if="capstone.screenshot_url" :src="capstone.screenshot_url" :alt="capstone.name">
     </div>
 
     <h2>Experience</h2>
-    <div v-for="experience_item in student.experience">
-      <h4>{{ experience_item.job_title }} | {{ experience_item.company_name }} | 
-      {{ experience_item.start_date }} - 
-      {{ experience_item.end_date }} </h4>
-      <p>{{ experience_item.details }}</p>
+    <div v-for="experience in student.experiences">
+      <h4>{{ experience.job_title }} | {{ experience.company_name }} | 
+      {{ experience.start_date }} - 
+      {{ experience.end_date }} </h4>
+      <p>{{ experience.details }}</p>
     </div>
 
 
     <h2>Education</h2>
-    <div v-for="education_item in student.education">
-      <h4>{{ education_item.degree }} | {{ education_item.university_name }} | 
-      {{ education_item.start_date }} - 
-      {{ education_item.end_date }} </h4>
-      <p>{{ education_item.details }}</p>
+    <div v-for="education in student.educations">
+      <h4>{{ education.degree }} | {{ education.university_name }} | 
+      {{ education.start_date }} - 
+      {{ education.end_date }} </h4>
+      <p>{{ education.details }}</p>
     </div>
 
     <h2>Twitter Feed</h2>
@@ -58,94 +58,94 @@
 import axios from "axios";
 import twitter from "vue-twitter";
 export default {
-  data: function() {
+  data: function () {
     return {
       student: {
         first_name: "Cheddar",
         last_name: "Cheese",
         email: "cheddar@gmail.com",
         phone_number: "1231231234",
-        short_bio: "I'm great in macaroni and cheese!",
+        bio: "I'm great in macaroni and cheese!",
         linkedin_url: "http://linkedin.com/in/cheddar",
         twitter_handle: "kraftcheese",
         github_url: "http://github.com/cheddar",
-        personal_url: "http://cheddar.com/",
-        image:
+        website_url: "http://cheddar.com/",
+        photo_url:
           "https://www.nutritionadvance.com/wp-content/uploads/2018/05/Cheddar-Cheese-101.jpg",
         skills: [
           { name: "Ruby" },
           { name: "Rails" },
           { name: "HTML" },
-          { name: "react" }
+          { name: "react" },
         ],
-        education: [
+        educations: [
           {
             start_date: "Jan. 1, 2019",
             end_date: "Feb. 22, 2019",
             degree: "Associates",
             university_name: "Community Cheese College",
-            details: "Studied development of cheese"
+            details: "Studied development of cheese",
           },
           {
-            start_date: "Mar. 1, 2019",
-            end_date: "Mar. 22, 2019",
+            start_date: "2019-03-01",
+            end_date: "2019-03-22",
             degree: "Bachelors",
             university_name: "Cheese College",
-            details: "Studied development of cheese"
+            details: "Studied development of cheese",
           },
           {
-            start_date: "May. 1, 2019",
-            end_date: "July. 28, 2019",
+            start_date: "2019-05-01",
+            end_date: "2019-07-22",
             degree: "Masters",
             university_name: "Cheese University",
-            details: "Master Cheese"
-          }
+            details: "Master Cheese",
+          },
         ],
-        experience: [
+        experiences: [
           {
-            start_date: "Aug.8, 2019",
-            end_date: "Sept. 22, 2019",
+            start_date: "2019-08-08",
+            end_date: "2019-09-22",
             job_title: "Cheese Developer",
             company_name: "Oberwise",
-            details: "Milked cows"
+            details: "Milked cows",
           },
           {
-            start_date: "Nov.8, 2019",
-            end_date: "Dec. 22, 2019",
+            start_date: "2019-11-22",
+            end_date: "2019-12-22",
             job_title: "Cheese Developer",
             company_name: "Dean's Dairy Farm",
-            details: "Formed cheese"
+            details: "Formed cheese",
           },
           {
-            start_date: "Jan.8, 2019",
-            end_date: "Feb. 22, 2019",
+            start_date: "2019-01-22",
+            end_date: "2020-01-22",
             job_title: "Cheese Engineer",
             company_name: "Horizon",
-            details: "Managed cheese makers"
-          }
+            details: "Managed cheese makers",
+          },
         ],
-        capstone: [
+        capstones: [
           {
             name: "Local Cheese App",
             description: "find local cheese sources based on your location",
             url: "http://wheredacheeseat.com",
-            screenshot: "https://cheeseapp.jpg"
+            screenshot_url:
+              "https://www.nutritionadvance.com/wp-content/uploads/2018/05/Cheddar-Cheese-101.jpg",
           },
           {
             name: "Say Cheese",
             description: "Cheese filters for cameras",
             url: "http://saycheese.com",
-            screenshot: "https://saycheese.jpg"
-          }
-        ]
-      }
+          },
+        ],
+      },
     };
   },
-  created: function() {
-    axios.get(`/api/students/${this.$route.params.id}`).then(response => {
+  created: function () {
+    axios.get(`/api/students/${this.$route.params.id}`).then((response) => {
       this.student = response.data;
     });
   },
-  methods: {}
+  methods: {},
 };
 </script>
